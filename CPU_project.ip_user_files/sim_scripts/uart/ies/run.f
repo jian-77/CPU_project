cc73523bb897147f7dd5ffef2c636cf753394413
ryan <2716564567@qq.com>
2024-05-07 15:32:31 +0800

    first commit

---
+++ CPU_project.ip_user_files/sim_scripts/uart/ies/run.f
@@ -0,0 +1,9 @@
+-makelib ies_lib/xil_defaultlib \
+  "../../../../CPU_project.srcs/sources_1/ip/uart/uart_bmpg.v" \
+  "../../../../CPU_project.srcs/sources_1/ip/uart/upg.v" \
+  "../../../../CPU_project.srcs/sources_1/ip/uart/sim/uart.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
